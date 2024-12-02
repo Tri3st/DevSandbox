8cba3fb0914edf809df39f9fc984292c42f03bcb
Martin van Diest <martin.van.diest@intemo.com>
2024-12-02 15:45:18 +0100

    Change stuff

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,10 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 from django.shortcuts import render
 from django.contrib.auth.models import Group, User
 from django.contrib.auth.decorators import login_required
 from rest_framework import permissions, viewsets
 
-from .models import Link, Post
+from .models import Link, Post, Tag
 from .serializers import PostSerializer, GroupSerializer, UserSerializer
 
 
@@ -56,3 +58,48 @@ class GroupViewSet(viewsets.ModelViewSet):
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+
+@csrf_exempt
+def post_list(request):
+    """
+    List all posts, or create a new post.
+    """
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def post_detail(request, pk):
+    """
+    Rerieve, update or delete a post.
+    """
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(post, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        post.delete()
+        return HttpResponse(status=204)
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,17 +2,4 @@
     <h1 class="text4xl">Welcome!</h1>
 </template>
 <script setup></script>
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
